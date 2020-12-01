e3dd8ec582966c1a534a5d624550e03ab00fa6e4
Adam Gruber <adamg@aweber.com>
2020-12-01 11:54:36 -0500

    Day 1

---
+++ day1.js
@@ -0,0 +1,51 @@
+const vm = require('./vm');
+
+const machine = vm({
+    inputFile: 'day1.txt',
+    dataTransformer: data => data.split('\n').map(parseFloat),
+});
+
+/**
+ * Given an array of values, find the values that, when added, produce the
+ * given sum.
+ * @param {number} sum Sum that numbers should produce
+ * @param {array} values Array of potential addends
+ * @param {number} numAddends Number of addends to find [default:2]
+ * @return {number}
+ */
+function findAddendsForSum(sum, values, numAddends) {
+    let addends = new Set();
+
+    for (let i = 0; i < values.length; i += 1) {
+        const addend1 = values[i];
+        const addend2 = sum - addend1;
+
+        if (numAddends > 2) {
+            Array.from(
+                findAddendsForSum(addend2, values, numAddends - 1)
+            ).forEach(addend => addends.add(addend));
+        } else {
+            if (values.includes(addend2)) {
+                addends.add(addend1);
+                addends.add(addend2);
+                break;
+            }
+        }
+    }
+
+    return addends;
+}
+
+function multiplySetItems(set) {
+    return Array.from(set).reduce((acc, val) => acc * val);
+}
+
+// Part 1
+const pt1 = findAddendsForSum(2020, machine.data);
+const pt1Solution = multiplySetItems(pt1);
+console.log(pt1, pt1Solution);
+
+// Part 2
+const pt2 = findAddendsForSum(2020, machine.data, 3);
+const pt2Solution = multiplySetItems(pt2);
+console.log(pt2, pt2Solution);
---
+++ vm.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+
+function sum(...args) {
+    return args.reduce((acc, val) => acc + val, 0);
+}
+
+class VM {
+    constructor(options) {
+        this.options = options;
+
+        this.readInput();
+    }
+
+    readInput() {
+        const { inputFile, dataTransformer } = this.options;
+
+        if (!inputFile) {
+            return;
+        }
+
+        const filePath = path.resolve(
+            path.join(__dirname, 'inputs', inputFile)
+        );
+
+        try {
+            this.data = fs.readFileSync(filePath, 'utf8');
+            if (dataTransformer) {
+                this.data = dataTransformer(this.data);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
+
+module.exports = options => {
+    const vm = new VM(options);
+    return vm;
+};
